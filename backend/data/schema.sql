d145ddb9feedf0557eaf4dd17b297a46f4541c1b
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-12-02 20:48:16 +0000

    redundant files and directories removed

--- backend/data/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE countries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    code TEXT UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    population INTEGER NOT NULL,
-    gdp REAL NOT NULL,
-    data_references TEXT
-);
